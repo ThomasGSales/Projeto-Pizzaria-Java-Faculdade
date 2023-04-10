38892e4de09574193c2998471e72c58f75c2e512
ssamoht <101594259+ThomasGSales@users.noreply.github.com>
2023-04-10 20:17:36 -0300

    Update Main.java

--- br/com/thomas/Main.java
+++ br/com/thomas/Main.java
@@ -89,9 +89,12 @@ public class Main {
 		
 		
 		
-		
+		// declaração da variavel taxa tipo float e iniciando ela com 0
 		float taxa = 0;
-				
+		
+		//Mais outra condição depois da classe pedido ser instanciada ->
+		// Condição que visa instanciar a classe de acordo com o tipo do pedido
+		// esses 2 tipos tem cada um sua propria classe
 		if(tipo.equals("Delivery")) {
 			
 			Delivery delivery = new Delivery(5.50f, pedido);
@@ -106,20 +109,35 @@ public class Main {
 		
 		
 		
-		
+		//criando um tipo Object para armazenar as String que serão as 2 oções do swing a seguir
+		//logo depois de declarar o tipo Object eu inicie o swing com as opções do proprio Object
+		// declarei uma variavel tipo >>inteira<< para armazenar a escolha do usuário
+		// declarei como inteiro pois o JOptionPane do tipo Option Dialog armazena a resposta com numeros inteiros apartir do 0
+		// ou seja se o usuário escolher a primeira opção ele vai armazenar o numero -> 0 
+		// e se escolher a segunda ele armazena o numero -> 1 (e assim por diante)
 		Object [] tipoPizza = {"Salgada", "Doce"}; 
 		int scanTipoSabor = JOptionPane.showOptionDialog(null, "Escolha o tipo da pizza:", "Pizzaria", 0, 3, null, tipoPizza, "");
 		
+		
+		
+		// Apartir daqui tive um pouco de dificuldade na hora do desenvolvimento >>
+		// não sei explicar muito bem teoricamente mas meio que instanciei um array da classe pizza
+		
 		Pizza[] pizzas = Pizza.getPizzas();
 		
+		// Aqui inicia a condição que envolve a variavel inteira de antes que armazenou a escolha do usuário
+		// a condição serve pra caso o usuário tenha escolhido as pizzas Salgadas -> apareça apenas as salgadas ou ao contrario
 		if(scanTipoSabor == 0) {
-	        Document.writee("Pizzas salgadas:");
-	        for (Pizza pizza : pizzas) {
-	            if (pizza.getTipo().equals("salgada")) {
-	                System.out.println(pizza.getCodigo() + " - " + pizza.getSabor() + " (" + pizza.getTipo() + "): R$" + pizza.getValor());
-	            }
-	        }
-		}
+	        	Document.writee("Pizzas salgadas:");
+			
+			/* ainda dentro da condição, eu inicio um for loop com uma condição dentro pra ele localizar apenas as pizzas na classe pizza 
+			com o atributo tipo = salgada e printar todas no console */
+	       		for (Pizza pizza : pizzas) {
+	            		if (pizza.getTipo().equals("salgada")) {
+	                	System.out.println(pizza.getCodigo() + " - " + pizza.getSabor() + " (" + pizza.getTipo() + "): R$" + pizza.getValor());
+	           		}	
+	       		}
+		} // no else é literalmente a mesma coisa caso ele escolha a opção que não retorne 0 (literalmente a unica que resta que é a doce)
 		else {
 	        Document.writee("Pizzas doces:");
 	        for (Pizza pizza : pizzas) {
@@ -133,26 +151,48 @@ public class Main {
 		
 		
 		
+		/* Após ele ver no console a lista de pizzas, ele tem que selecionar uma né? então aqui eu começo esse processo,
+		confesso que o metodo que eu usei não é o melhor visto que vou usar o swing junto com o console, mas eu pretendo melhorar em breve
+		essa parte */
 		
+		// começo essa etapa inicializando a string pizzaSelecionada e o tipo float precoPizza
 		String pizzaSelecionada = null;
 		float precoPizza = 0;
 		
+		//inicio aqui um while loop pra ele recomeçar todo o processo caso o usuário clique em ok sem informar nada no input
 		while(pizzaSelecionada==null) {
 			
+			//Já dentro do loop, o inicio do processo é declarando uma String chamada scanCod que vai armazenar o input do usuário
 			String scanCod;
 			scanCod = JOptionPane.showInputDialog(null, "Veja a lista de pizzas no console e escolha o código da pizza que quiser");
+			//nesse momento a usuário terá que observar a lista de pizzas no console e informar o numero da pizza que ele desejar
+			// eu usei tipo String para armazenar o código porque esse tipo de JOption aparentemente só armazena string 
+			// mas eu fiz uma gambiarra para arrumar isso depois
 		
+			
+			// ainda dentro do loop eu abro outro loop só que em for kkkkkk
+			// mas com o esquema parecido com o usado pra printar as pizzas na tela antes
 			for(Pizza pizza: pizzas) {
+				
+				//abro uma condição dentro do for para ele puxar o codigo da pizza que for igual o numero que o usuário informou
+				// como a variavel era tipo String, eu usei o Integer.parseInt para tornala em Inteira nessa comparação
 				if(pizza.getCodigo() == Integer.parseInt(scanCod)) {
+					
+					// caso a condição seja verdadeira ele vai armazenar o atributo sabor da mesma em pizzaSelecionada
+					// e também o atributo valor dela em precoPizza
 					pizzaSelecionada = pizza.getSabor();
 					precoPizza = pizza.getValor();
 					break;
 				}
 			}
+			
+			/*	depois dos processos anteriores temos outra condição para caso tenha dado tudo certo ele vai verificar se tem algo
+			armazenado na variavel pizzaSelecionada e se tiver ele vai setar o atributo que ela armazenou no atributo pizza da classe
+			pedido    */
 			if(pizzaSelecionada != null) {
 				pedido.setPizza(pizzaSelecionada);
 				Document.alert("Você escolheu a pizza: "+ pizzaSelecionada);
-			} else {
+			} else { // mas caso o usuário não tenha escolhido nenhnuma pizza anteriormente, antes do loop começar denovo, ele vai avisar o erro
 				Document.alert("pizza não encontrada");
 				
 			}
